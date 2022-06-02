5f4f52436f54678fcf35491caf93f6d04a800db1
Gustavo <luiz.gustavo.sp40@gmail.com>
2022-06-02 10:17:51 -0300

    Projeto lista de afazeres

--- javaScript/introducao-ao-javascript/to-do list/assets/scripts.js
+++ javaScript/introducao-ao-javascript/to-do list/assets/scripts.js
@@ -1,16 +1,16 @@
-const form = document.getElementById('task-form');
-const taskList = document.getElementById('tasks');
+const form = document.getElementById('idFormTarefa');      //Pega Formulario tarefa 
+const tarefa = document.getElementById('idRecebeTarefa');  //Pega a tarefa adicionada
 
-form.onsubmit = function (e) {
-	e.preventDefault();
-	const inputField = document.getElementById('task-input');
-	addTask(inputField.value);
-	form.reset();
+form.onsubmit = function (e) {    							//Quando tiver um submit executa a função
+	e.preventDefault();          							//cancela o evento se for cancelável,a ação padrão que pertence ao evento não ocorrerá. Ex:Nao deixa a pagina atualizar apagando as informaçoes
+	const tarefa = document.getElementById('idAddTarefa');  //Atribui o imput a var tarefa
+	addTarefa(tarefa.value);      							//Metodo recebe parametro e argumento/valor
+	form.reset();                 							//Limpa a caixa de texto do formulario
 };
 
-function addTask(description) {
+function addTarefa(description) {
 	const taskContainer = document.createElement('div');
-	const newTask = document.createElement('input');
+	const newTask = document.createElement('input');   		
 	const taskLabel = document.createElement('label');
 	const taskDescriptionNode = document.createTextNode(description);
 
@@ -25,5 +25,5 @@ function addTask(description) {
 	taskContainer.appendChild(newTask);
 	taskContainer.appendChild(taskLabel);
 
-	taskList.appendChild(taskContainer);
+	tarefa.appendChild(taskContainer);
 }
