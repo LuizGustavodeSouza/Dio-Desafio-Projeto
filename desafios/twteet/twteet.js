d3b121cfbdb4a1ecfb4a5d16ceba7bf619c575a8
Gustavo <luiz.gustavo.sp40@gmail.com>
2022-06-20 14:31:17 -0300

    Desafios

---
+++ desafios/twteet/twteet.js
@@ -0,0 +1,16 @@
+//entrada Com 140 caracter
+//let T = "RT @TheEllenShow: If only Bradley's arm was longer. Best photo ever. #oscars pic.twitter.com/C9U5NOtGap";
+
+//entrada Com + de 140 caracter
+let T = "1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
+
+
+console.log((T.length <= 140 ) ?  "TWEET" :"MUTE"  );
+/*
+if(T.length <=140 ){
+    console.log("TWEET");
+}
+else{
+    console.log("MUTE");
+}
+*/
---
+++ desafios/vetor/vetor.js
@@ -0,0 +1,7 @@
+var V = 49; 
+//var mult = 0;
+for(let i=0; i<10; i++){
+    console.log(`N[${i}] = ${V}`);
+    V = (V*1)+V;
+    
+}
