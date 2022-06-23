c04176fc90e0b34f9633a65e45d29704bee9d399
Gustavo <luiz.gustavo.sp40@gmail.com>
2022-06-23 09:36:51 -0300

    react

---
+++ react/github-api-interface-main/src/components/darkMode/DarkMode.js
@@ -0,0 +1,54 @@
+//import React from 'react'
+
+
+
+
+function DarkMode() {
+  return (
+	<>
+		<button type="submit" onClick={DarkMode}>
+          <span>Dark Mode</span>
+        </button>
+	</>
+  );
+}
+
+export default DarkMode
+
+
+
+/*
+function changeMode() {
+	changeClasses();
+	changeText();
+}
+
+function changeClasses() {	// Altera a clase para Dark Mode
+	button.classList.toggle(darkModeClass);
+	h1.classList.toggle(darkModeClass);
+	body.classList.toggle(darkModeClass);
+	footer.classList.toggle(darkModeClass);
+}
+
+function changeText() {  //Altera texto do botão
+	const lightMode = 'Light Mode';
+	const darkMode = 'Dark Mode';
+
+	if (body.classList.contains(darkModeClass)) {
+		button.innerHTML = lightMode;
+		h1.innerHTML = darkMode + ' ON';
+		return;
+	}
+
+	button.innerHTML = darkMode;
+	h1.innerHTML = lightMode + ' ON';
+}
+
+const darkModeClass = 'dark-mode';
+const button = document.getElementById('mode-selector');
+const h1 = document.getElementById('page-title');
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
+
+button.addEventListener('click', changeMode);
+*/
